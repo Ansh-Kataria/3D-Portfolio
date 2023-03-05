67600bbcc90bf07d9983210db02cab40f6f64da4
Ansh Kataria <anshkataria28@gmail.com>
2023-03-05 11:38:42 +0530

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+// currently here
 const About = () => {
   return (
     <div>About</div>
